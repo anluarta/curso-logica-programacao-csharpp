7483233f3348209e0595d18c34ce400ce8e58875
Andre Luis Araujo Tamboril <anluarta@hotmail.com>
2019-01-10 09:37:51 -0200

    primeira atualizacao curso csharp

---
+++ Ola Mundo/Ola Mundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ola_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo");
+            Console.ReadLine();
+        }
+    }
+}
